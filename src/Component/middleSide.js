c0a4c6bae49e95eb1e16fd919a7ca9a7d4cea478
alkamisiddik <asiddik@tulip-tech.com>
2025-09-18 18:45:23 +0600

    completed with core css (static post and suggested)

--- src/Component/middleSide.js
+++ src/Component/middleSide.js
@@ -1,11 +1,17 @@
 import "./middleSide.css";
 import stories from "../storys.json";
 import Post from "./Post/post";
+import instagramLogo from '../assets/instagramlogo.png';
 
 const MiddleSide = () => {
   const story = stories.story;
   return (
     <div className="middleHomeSide">
+      <div className="mobileTopBar">
+        <div className="instaMobileLogo">
+          <img className="logoImg" src={instagramLogo} alt="Instagram" />
+        </div>
+      </div>
       <div className="storyBlock">
         {story?.map((item, index) => {
           return (
--- src/Component/rightSide.js
+++ src/Component/rightSide.js
@@ -1,16 +1,99 @@
-import "./rigjhtSide.css"
-import React from 'react'
+import "./rigjhtSide.css";
+import React from "react";
 
 const RightSide = () => {
   return (
     <div className="rightSideHome">
       <div className="topProfileRight">
-        <div className="imageDivRightSide">
-            <img className="imageRightSideProfile" src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="Profile"/>
+        <div className="leftRightProfile">
+          <div className="imageDivRightSide">
+            <img
+              className="imageRightSideProfile"
+              src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+              alt="Profile"
+            />
+          </div>
+          <div className="usernameBlock">
+            <div className="usernameRightSide">__rumman</div>
+            <div className="userFullName">Alkami Siddik</div>
+          </div>
+        </div>
+        <div className="switchBtn">Switch</div>
+      </div>
+      <div className="bottomRightSide">
+        <div className="suggestedBlock">
+          <div className="suggestForYou">Suggested for you</div>
+          <div className="seeAll">See All</div>
+        </div>
+        <div className="followBlockRightSide">
+          <div className="profileRightBottom">
+            <div className="leftRightProfile">
+              <div className="imageDivRightSide">
+                <img
+                  className="imageRightSideProfile"
+                  src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+                  alt="Profile"
+                />
+              </div>
+              <div className="usernameBlock">
+                <div className="usernameRightSide">__rumman</div>
+                <div className="userFullName">Alkami Siddik</div>
+              </div>
+            </div>
+            <div className="switchBtn">Follow</div>
+          </div>
+          <div className="profileRightBottom">
+            <div className="leftRightProfile">
+              <div className="imageDivRightSide">
+                <img
+                  className="imageRightSideProfile"
+                  src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+                  alt="Profile"
+                />
+              </div>
+              <div className="usernameBlock">
+                <div className="usernameRightSide">__rumman</div>
+                <div className="userFullName">Alkami Siddik</div>
+              </div>
+            </div>
+            <div className="switchBtn">Follow</div>
+          </div>
+          <div className="profileRightBottom">
+            <div className="leftRightProfile">
+              <div className="imageDivRightSide">
+                <img
+                  className="imageRightSideProfile"
+                  src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+                  alt="Profile"
+                />
+              </div>
+              <div className="usernameBlock">
+                <div className="usernameRightSide">__rumman</div>
+                <div className="userFullName">Alkami Siddik</div>
+              </div>
+            </div>
+            <div className="switchBtn">Follow</div>
+          </div>
+          <div className="profileRightBottom">
+            <div className="leftRightProfile">
+              <div className="imageDivRightSide">
+                <img
+                  className="imageRightSideProfile"
+                  src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+                  alt="Profile"
+                />
+              </div>
+              <div className="usernameBlock">
+                <div className="usernameRightSide">__rumman</div>
+                <div className="userFullName">Alkami Siddik</div>
+              </div>
+            </div>
+            <div className="switchBtn">Follow</div>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RightSide
+export default RightSide;
